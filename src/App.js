c1fa57ba26639cc990d37ead7598d0faeb797a22
Mamadou Diaw <diawmamadou057@gmail.com>
2022-08-30 13:47:53 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <div className='col-md-8 offset-2 mt-5'>
+          <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default App;
